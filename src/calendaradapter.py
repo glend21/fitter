c620945a807efe67377be68ea7deafd1ba1a4ebb
Glen Downton <gidownton@gmail.com>
2021-02-09 21:19:49 +0800

    adding calendar adapter

---
+++ src/calendaradapter.py
@@ -0,0 +1,18 @@
+'''
+    GD 20210209
+
+    Adapter to allow the calendar component to communicate with the server page
+'''
+
+import calendar
+
+
+class CalendarAdapter():
+    ''' Calendar adapter '''
+
+    def __init__( self ):
+        ''' Ctor '''
+        pass
+
+
+    
